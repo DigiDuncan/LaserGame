cb5003ae2d74c78accef095dd31bd11dacb84ff0
DigiDuncan <digiduncan@gmail.com>
2020-06-17 18:03:05 -0400

    Templating!

---
+++ lasergame/lib/constants.py
@@ -0,0 +1,32 @@
+import importlib.resources as pkg_resources
+
+import toml
+
+import lasergame.data
+
+ids = None
+emojis = None
+colors = None
+
+
+class AttrDict:
+    def __init__(self, data):
+        self.__dict__ = data
+
+
+def loadgame(data):
+    global game
+    gamedict = data.get("game", {})
+    # make all names lowercase
+    gamedict = {name.lower(): value for name, value in gamedict.items()}
+    # create the enum
+    game = AttrDict(gamedict)
+
+
+def load():
+    # Load constants toml file
+    data = toml.loads(pkg_resources.read_text(lasergame.data, "constants.toml"))
+    loadgame(data)
+
+
+load()
