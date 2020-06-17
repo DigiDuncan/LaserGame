55c93394e22abe688d2897800cc3491a9cfbf847
DigiDuncan <digiduncan@gmail.com>
2020-06-17 19:49:54 -0400

    Got basic tests working.

--- lasergame/lib/constants.py
+++ lasergame/lib/constants.py
@@ -3,13 +3,10 @@ import importlib.resources as pkg_resources
 import toml
 
 import lasergame.data
+from lasergame.lib.attrdict import AttrDict
 
 game = None
-
-
-class AttrDict:
-    def __init__(self, data):
-        self.__dict__ = data
+buttons = None
 
 
 def loadgame(data):
@@ -19,12 +16,25 @@ def loadgame(data):
     gamedict = {name.lower(): value for name, value in gamedict.items()}
     # create the enum
     game = AttrDict(gamedict)
+    game["width"] = game.basewidth * game.scale
+    game["height"] = game.baseheight * game.scale
+    game["center"] = (round(game.width / 2), round(game.height / 2))
+
+
+def loadbuttons(data):
+    global buttons
+    buttonsdict = data.get("buttons", {})
+    # make all names lowercase
+    buttonsdict = {name.lower(): value for name, value in buttonsdict.items()}
+    # create the enum
+    buttons = AttrDict(buttonsdict)
 
 
 def load():
     # Load constants toml file
     data = toml.loads(pkg_resources.read_text(lasergame.data, "constants.toml"))
     loadgame(data)
+    loadbuttons(data)
 
 
 load()
--- lasergame/procedures/gameloop.py
+++ lasergame/procedures/gameloop.py
@@ -1,2 +1,40 @@
+import pygame
+from pygame import locals
+
+import digicolor
+
+from lasergame.lib.attrdict import AttrDict
+from lasergame.lib.constants import game
+from lasergame.lib.constants import buttons as keys
+
+colors = digicolor.colors
+buttonsdict = {}
+for k, v in keys._values.items():
+    buttonsdict[k] = getattr(locals, v)
+buttons = AttrDict(buttonsdict)
+
+
 def gameloop():
-    pass
+    pygame.init()
+
+    # Create the screen.
+    screen = pygame.display.set_mode([game.width, game.height])
+
+    running = True
+
+    while running:
+        # Window close button
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+        # Fill the background with white
+        screen.fill(colors.BLACK.rgb)
+
+        # Draw a solid blue circle in the center
+        pygame.draw.circle(screen, (0, 0, 255), (game.center[0], game.center[1]), 75)
+
+        # Flip [refresh?] the display
+        pygame.display.flip()
+
+
+pygame.quit()
