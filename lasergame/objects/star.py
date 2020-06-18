a3f8acc2579d6ebc9277b4b349d28d61ce9b631f
Natalie Fearnley <nfearnley@gmail.com>
2020-06-18 16:39:50 -0400

    Added star twinkles

--- lasergame/objects/star.py
+++ lasergame/objects/star.py
@@ -1,3 +1,5 @@
+import random
+
 import pygame
 
 from digicolor import colors
@@ -6,9 +8,21 @@ from lasergame.objects.gameobject import GameObject
 
 
 class Star(GameObject):
+    colors = [colors.LIGHT_GRAY.rgb, colors.WHITE.rgb, colors.LIGHT_YELLOW.rgb, colors.YELLOW.rgb]
+    twinkle_wait = 64
+
     def __init__(self, x, y):
         self.x = x
         self.y = y
+        self.twinkle = random.randint(0, self.twinkle_wait * 4)
+
+    def update(self, events):
+        self.twinkle = (self.twinkle + 1) % (self.twinkle_wait * 4)
+
+    @property
+    def color(self):
+        color_index = int(self.twinkle / self.twinkle_wait)
+        return self.colors[color_index]
 
     def draw(self, screen):
-        pygame.draw.circle(screen, colors.WHITE.rgb, (self.x, self.y), 2)
+        pygame.draw.circle(screen, self.color, (self.x, self.y), 2)
