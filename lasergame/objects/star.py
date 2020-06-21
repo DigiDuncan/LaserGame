31d73327378d211d2f80d5520ed2909a4799bccd
DigiDuncan <digiduncan@gmail.com>
2020-06-20 21:36:38 -0400

    Make star size an argument, JIC?

--- lasergame/objects/star.py
+++ lasergame/objects/star.py
@@ -11,8 +11,9 @@ class Star(GameObject):
     colors = [colors.LIGHT_GRAY.rgb] * 300 + [colors.WHITE.rgb] * 4 + [colors.LIGHT_YELLOW.rgb] * 4 + [colors.YELLOW.rgb]
     color_speed = 8
 
-    def __init__(self, center):
+    def __init__(self, center, size = 2):
         self.twinkle = random.random() * len(self.colors)
+        self.size = size
         super().__init__(center=center)
 
     def update(self, clock, **kwargs):
@@ -24,4 +25,4 @@ class Star(GameObject):
         return self.colors[color_index]
 
     def draw(self, screen):
-        pygame.draw.circle(screen, self.color, (self.x, self.y), 2)
+        pygame.draw.circle(screen, self.color, (self.x, self.y), self.size)
