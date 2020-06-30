5782b3ad05f551762c13ef9eb743f872ea2964fe
DigiDuncan <digiduncan@gmail.com>
2020-06-29 20:06:03 -0400

    Fix clamp.

--- lasergame/lib/utils.py
+++ lasergame/lib/utils.py
@@ -1,4 +1,10 @@
 def clamp(minVal, val, maxVal):
+    if minVal is None and maxVal is None:
+        return val
+    if minVal is None:
+        return max(val, maxVal)
+    if maxVal is None:
+        return min(minVal, val)
     return max(minVal, min(maxVal, val))
 
 
