72d6ac3fb2940fa19e264e2b770b9240b4bcc15a
DigiDuncan <digiduncan@gmail.com>
2020-07-06 05:29:20 -0400

    Comment the bullshit that is framegraph.

--- lasergame/objects/framegraph.py
+++ lasergame/objects/framegraph.py
@@ -6,6 +6,12 @@ from lasergame.lib.utils import clamp
 
 
 colorlist = []
+
+# This genenerates a list of rgb tuples fading between red to gree,
+# passing through orange and yellow. It starts at (255, 0, 0)
+# [or (0xFF, 0x00, 0x00)], fades G from 0 -> 255 for 30 colors,
+# then fades R from 255 -> 0 for 30 colors, resulting in 61 colors,
+# from index 0 (red) to index 60 (green).
 for i in range(61):
     r = 0
     g = 0
@@ -39,6 +45,10 @@ class FrameGraph(GameObject):
         self._fps = int(clock.get_fps())
 
     def draw(self, debugscreen, **kwargs):
+        # Every tick, scroll the surface on pixel to the left,
+        # then place a new pixel at the top-right for 60 FPS or above,
+        # and one pixel down from the top for every frame under 60
+        # the current FPS is.
         if not self.show:
             return
         self.surface.scroll(dx=-1)
