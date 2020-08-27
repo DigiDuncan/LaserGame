e3098da853441d0922d9be8a7f12c6d091fa0e55
DigiDuncan <digiduncan@gmail.com>
2020-08-26 21:28:48 -0400

    Switch to ~ for debug toggle.

--- lasergame/classes/inputlistener.py
+++ lasergame/classes/inputlistener.py
@@ -5,6 +5,6 @@ class InputListener(GameObject):
     __slots__ = []
 
     def update(self, gm, events, **kwargs):
-        # If SELECT and START are pressed, debug toggle.
-        if (gm.input.SELECT.pressed or gm.input.START.pressed) and (gm.input.SELECT.held and gm.input.START.held):
+        # If `/~ pressed, bedug toggle.
+        if gm.input.DEBUG.pressed:
             gm.state.debug = not gm.state.debug
